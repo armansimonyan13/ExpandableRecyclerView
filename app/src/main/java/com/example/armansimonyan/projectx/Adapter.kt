c03f227fe513ec635be1fb2e28e392ab34cb3c85
armansimonyan <armansimonyan@workfront.com>
2017-10-19 01:42:08 +0400

    Rewrote Adapter in Kotlin

---
+++ app/src/main/java/com/example/armansimonyan/projectx/Adapter.kt
@@ -0,0 +1,105 @@
+package com.example.armansimonyan.projectx
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+/**
+ * Created by armansimonyan.
+ */
+
+open class Adapter(context: Context, val data: List<GroupItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), View.OnClickListener {
+
+	companion object {
+		val GROUP_TYPE = 0
+		val CHILD_TYPE = 1
+	}
+
+	private var inflater: LayoutInflater = LayoutInflater.from(context)
+
+	override fun getItemCount(): Int {
+		var count = 0
+		for (groupItem in data) {
+			count++
+			if (groupItem.isExpanded) {
+				count += groupItem.items.size
+			}
+		}
+		return count
+	}
+
+	override fun getItemViewType(position: Int): Int = when (getItem(position)) {
+		is GroupItem -> GROUP_TYPE
+		is ChildItem -> CHILD_TYPE
+		else -> throw IllegalStateException("Unknown type")
+	}
+
+	private fun getItem(position: Int) : Any {
+		var i = 0
+		for (groupItem in data) {
+			if (i == position) {
+				return groupItem
+			}
+			i++
+			if (groupItem.isExpanded) {
+				for (childItem in groupItem.items) {
+					if (i == position) {
+						return childItem
+					}
+					i++
+				}
+			}
+		}
+		throw IllegalStateException("Item not found for position $position")
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+		return when (viewType) {
+			GROUP_TYPE -> GroupViewHolder(inflater.inflate(R.layout.view_item_group, parent, false))
+			CHILD_TYPE -> ChildViewHolder(inflater.inflate(R.layout.view_item_child, parent, false))
+			else -> throw IllegalStateException("Unknown view type $viewType")
+		}
+	}
+
+	override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) =
+			if (holder is GroupViewHolder) {
+				holder.imageView.setOnClickListener(this)
+				holder.imageView.tag = holder
+				val groupItem = getItem(position) as GroupItem
+				holder.textView.text = groupItem.name
+				if (groupItem.isExpanded) {
+					holder.imageView.rotation = 90F
+				} else {
+					holder.imageView.rotation = 0F
+				}
+			} else if (holder is ChildViewHolder) {
+				holder.itemView.setOnClickListener(this)
+				holder.itemView.tag = holder
+				holder.textView.text = (getItem(position) as ChildItem).name
+			} else {
+				throw IllegalArgumentException("Holder is of unrecognized type: ${holder!!::class.java}")
+			}
+
+	override fun onClick(v: View?) {
+
+	}
+
+	class GroupViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+		var textView : TextView = itemView.findViewById(R.id.text)
+		var imageView : ImageView = itemView.findViewById(R.id.arrow)
+	}
+
+	class ChildViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+		var textView : TextView = itemView.findViewById(R.id.text)
+	}
+
+	@Suppress("unused")
+	fun log(message: String, vararg args: Any) {
+		Log.d("Adapter", String.format(message, args))
+	}
+}
--- app/src/main/java/com/example/armansimonyan/projectx/MainActivity.java
+++ app/src/main/java/com/example/armansimonyan/projectx/MainActivity.java
@@ -1,16 +1,9 @@
 package com.example.armansimonyan.projectx;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 			data.add(groupItem);
 		}
 
-		RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
+		RecyclerView recyclerView = findViewById(R.id.recycler);
 		recyclerView.setLayoutManager(new StickyHeaderLayoutManager());
 		DefaultItemAnimator itemAnimator = new DefaultItemAnimator();
 		itemAnimator.setRemoveDuration(1000);
@@ -44,149 +37,4 @@ public class MainActivity extends AppCompatActivity {
 		recyclerView.setAdapter(new Adapter(this, data));
 	}
 
-	public static class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements View.OnClickListener {
-		public static final int GROUP_TYPE = 0;
-		public static final int CHILD_TYPE = 1;
-
-		private LayoutInflater inflater;
-		private List<GroupItem> data;
-
-		public Adapter(Context context, List<GroupItem> data) {
-			inflater = LayoutInflater.from(context);
-			this.data = data;
-		}
-
-		@Override
-		public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-			if (viewType == GROUP_TYPE) {
-				return new GroupViewHolder(inflater.inflate(R.layout.view_item_group, parent, false));
-			} else if (viewType == CHILD_TYPE) {
-				return new ChildViewHolder(inflater.inflate(R.layout.view_item_child, parent, false));
-			} else {
-				throw new IllegalArgumentException("Unknown view type: " + viewType);
-			}
-		}
-
-		@Override
-		public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-			if (holder instanceof GroupViewHolder) {
-				GroupViewHolder groupViewHolder = (GroupViewHolder) holder;
-				groupViewHolder.itemView.setOnClickListener(this);
-				groupViewHolder.itemView.setTag(groupViewHolder);
-				GroupItem groupItem = (GroupItem) getItem(position);
-				groupViewHolder.textView.setText(groupItem.getName());
-				if (groupItem.isExpanded()) {
-					groupViewHolder.imageView.setRotation(90);
-				} else {
-					groupViewHolder.imageView.setRotation(0);
-				}
-			} else if (holder instanceof ChildViewHolder) {
-				ChildViewHolder childViewHolder = (ChildViewHolder) holder;
-				childViewHolder.itemView.setOnClickListener(this);
-				childViewHolder.itemView.setTag(childViewHolder);
-				childViewHolder.textView.setText(((ChildItem) getItem(position)).getName());
-			} else {
-				throw new IllegalArgumentException("Holder is of unrecognized type: " + holder.getClass());
-			}
-		}
-
-		@Override
-		public int getItemCount() {
-			int count = 0;
-			for (GroupItem groupItem : data) {
-				count++;
-				if (groupItem.isExpanded()) {
-					count += groupItem.getItems().size();
-				}
-			}
-			return count;
-		}
-
-		@Override
-		public int getItemViewType(int position) {
-			Object item = getItem(position);
-			if (item instanceof GroupItem) {
-				return GROUP_TYPE;
-			} else if (item instanceof ChildItem) {
-				return CHILD_TYPE;
-			} else {
-				throw new IllegalStateException("Unknown type");
-			}
-		}
-
-		@Override
-		public void onClick(View view) {
-			Object holder = view.getTag();
-			if (holder instanceof GroupViewHolder) {
-				GroupViewHolder groupViewHolder = (GroupViewHolder) holder;
-				log("adapterPosition: " + groupViewHolder.getAdapterPosition());
-				log("layoutPosition: " + groupViewHolder.getLayoutPosition());
-				int adapterPosition = groupViewHolder.getAdapterPosition();
-				Object item = getItem(adapterPosition);
-				if (item instanceof GroupItem) {
-					GroupItem groupItem = (GroupItem) item;
-					if (groupItem.isExpanded()) {
-						groupItem.setIsExpanded(false);
-						groupViewHolder.imageView.setRotation(0);
-						notifyItemRangeRemoved(adapterPosition + 1, groupItem.getItems().size());
-					} else {
-						groupItem.setIsExpanded(true);
-						groupViewHolder.imageView.setRotation(90);
-						notifyItemRangeInserted(adapterPosition + 1, groupItem.getItems().size());
-					}
-				}
-			}
-		}
-
-		public Object getItem(int position) {
-			int i = 0;
-			for (GroupItem groupItem : data) {
-				if (i == position) {
-					return groupItem;
-				}
-				i++;
-				if (groupItem.isExpanded()) {
-					for (ChildItem childItem : groupItem.getItems()) {
-						if (i == position) {
-							return childItem;
-						}
-						i++;
-					}
-				}
-			}
-			return null;
-		}
-	}
-
-	public static class GroupViewHolder extends RecyclerView.ViewHolder {
-		public TextView textView;
-		public ImageView imageView;
-
-		public GroupViewHolder(View itemView) {
-			super(itemView);
-
-			textView = (TextView) itemView.findViewById(R.id.text);
-			imageView = (ImageView) itemView.findViewById(R.id.arrow);
-		}
-	}
-
-	public static class ChildViewHolder extends RecyclerView.ViewHolder {
-		public TextView textView;
-
-		public ChildViewHolder(View itemView) {
-			super(itemView);
-
-			textView = (TextView) itemView.findViewById(R.id.text);
-		}
-	}
-
-	private static void log(String message) {
-		Log.d("MainActivity>>>: ", message);
-	}
-
-	private static void log() {
-		Log.d("MainActivity>>>: \n",
-				""
-		);
-	}
 }
